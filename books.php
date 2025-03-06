bbf02af9de7aafbe8e8834ba824a07b18781b45c
Jazib <jazib1khan@gmail.com>
2025-03-06 17:18:51 +0000

    Handling pagination

--- books.php
+++ books.php
@@ -5,14 +5,28 @@ require 'vendor/autoload.php';
 use Goutte\Client;
 
 function scrapePage($url, $client){
-    $crawler = $client->request('GET', url);
+    $crawler = $client->request('GET', $url);
     $crawler->filter('.product_pod')->each(function ($node){
         $title = $node->filter('.image_container img')->attr('alt');
         $price = $node->filter('.price_color')->text();
 
         echo $title . "-" . $price . PHP_EOL;
     });
+
+    try {
+        $next_page = $crawler->filter('.next > a')->attr('href');
+    } catch(InvalidArgumentException) {
+        return null;
+    }
+
+    return "https://books.toscrape.com/catalogue/" . $next_page;
 }
 
+$client = new Client();
+$nextUrl = 'https://books.toscrape.com/catalogue/page-1.html';
+
+while ($nextUrl) {
+    $nextUrl = scrapePage($nextUrl, $client);
+}
 
 ?>
