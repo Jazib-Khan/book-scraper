3c93b4981e2694bad7d3e9f43ab0452d2b1d53e5
Jazib <jazib1khan@gmail.com>
2025-03-06 17:23:48 +0000

    Writing data to a CSV file

--- books.php
+++ books.php
@@ -4,13 +4,15 @@ require 'vendor/autoload.php';
 
 use Goutte\Client;
 
-function scrapePage($url, $client){
+function scrapePage($url, $client, $file) {
     $crawler = $client->request('GET', $url);
-    $crawler->filter('.product_pod')->each(function ($node){
+    $crawler->filter('.product_pod')->each(function ($node) use ($file) {
         $title = $node->filter('.image_container img')->attr('alt');
         $price = $node->filter('.price_color')->text();
 
         echo $title . "-" . $price . PHP_EOL;
+
+        fputcsv($file, [$title, $price]);
     });
 
     try {
@@ -23,10 +25,13 @@ function scrapePage($url, $client){
 }
 
 $client = new Client();
+$file = fopen("books.csv", "a");
 $nextUrl = 'https://books.toscrape.com/catalogue/page-1.html';
 
 while ($nextUrl) {
-    $nextUrl = scrapePage($nextUrl, $client);
+    $nextUrl = scrapePage($nextUrl, $client, $file);
 }
+
+fclose($file);
 
 ?>
