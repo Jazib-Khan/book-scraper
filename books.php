6030282d35a664c0c8664ef3e1890eb5b871b215
Jazib <jazib1khan@gmail.com>
2025-03-06 17:04:53 +0000

    Sending HTTP requests with Goutte

---
+++ books.php
@@ -0,0 +1,13 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Goutte\Client;
+
+$client = new Client();
+
+$crawler = $client->request('GET', 'https://books.toscrape.com/');
+
+echo $crawler->html();
+
+?>
